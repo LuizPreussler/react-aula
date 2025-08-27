4f79c098729101265ccd66c71f7b3ebb63ab5c4e
react aula <luiz.epreussler@gmail.com>
2025-08-27 10:56:22 -0300

    innitial commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+interface ICardProps {
+  title: string;
+  fontSize: string;
+  headerSize: string;
+  children: React.ReactNode;
+}
+
+const Card = (props: ICardProps) => {
+  return (
+    <div style={{ border: "1px solid black", fontSize: Number(props.fontSize) }}>
+      <span>Title: {props.title}</span>
+      <div>Footer </div>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <>
+    <div>
+      Test
+      <p>Card(s):</p>
+      <Card title='Teste-1' fontSize='8'><h1>Exemplo</h1></Card>
+      <Card title='Teste-2' fontSize='16'><h2>Exemplo</h2></Card>
+      <Card title='Teste-3' fontSize='32'><h3>Exemplo</h3></Card>
+      <Card title='Teste-4' fontSize='64'><h4>Exemplo</h4></Card>
+    </div>
+    </>
+  )
+}
+
+export default App
